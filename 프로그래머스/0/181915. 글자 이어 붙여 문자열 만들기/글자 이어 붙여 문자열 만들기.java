0831e85c350e6e1701d17cc3fc8d8f72e2cc0e81
정병산불곰 <119942444+bulgom2@users.noreply.github.com>
2024-05-07 10:38:22 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 4.12 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,15 @@
+
+
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+
+        String[] arr = my_string.split("");
+        String answer = "";
+        
+        for (int i = 0; i < index_list.length; i++) {
+            answer += arr[index_list[i]];   
+        }
+        
+        return answer;
+    }
+}
