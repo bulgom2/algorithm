c22950b068d4a3300122e16c927d17857dab5654
정병산불곰 <119942444+bulgom2@users.noreply.github.com>
2024-10-31 19:51:57 +0900

    다른 방법 추가

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,8 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        // 배열을 계속 업데이트하면 효율이 떨어질 수 있으므로
-        // 리스트로 변환하여 작업
+
+        // 기존 배열 업데이트 방법
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                // 짝수 인덱스에서는 query[i] 이후의 요소들을 잘라냄
+                arr = Arrays.copyOfRange(arr, 0, query[i] + 1);
+            } else {
+                // 홀수 인덱스에서는 query[i] 이전의 요소들을 잘라냄
+                arr = Arrays.copyOfRange(arr, query[i], arr.length);
+            }
+        }
+        
+        return arr;
+
+        // 리스트로 변환하여 푸는 방법
         List<Integer> list = new ArrayList<>();
         for (int num : arr) {
             list.add(num);
